c8845fdec9643c3c4db9b5c86394d70467601f4f
Matt Tilghman <mtilghman03@gmail.com>
2025-04-30 15:21:05 -0400

    Working Version

--- multiple.py
+++ multiple.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 #Loading the Dataset
 data_url = "http://lib.stat.cmu.edu/datasets/boston"
-raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+raw_df = pd.read_csv(data_url, sep="\\s+", skiprows=22, header=None)
 
 #Data Preprocessing
 X = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
